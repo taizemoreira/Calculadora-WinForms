7e1e16e3a1dd0fd1299f9bc41b201a9ab65f886d
Taize Moreira <taizebueno@uniplaclages.edu.br>
2023-05-14 11:59:14 -0300

    Atualizacao

--- CalculadoraWinForms/Form1.Designer.cs
+++ CalculadoraWinForms/Form1.Designer.cs
@@ -28,12 +28,376 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            tableLayoutPanel1 = new TableLayoutPanel();
+            btn7 = new Button();
+            btn8 = new Button();
+            btn9 = new Button();
+            btnDividir = new Button();
+            btn4 = new Button();
+            btn5 = new Button();
+            btn6 = new Button();
+            btnMultiplicar = new Button();
+            btn1 = new Button();
+            btn3 = new Button();
+            btn2 = new Button();
+            btnSubtrair = new Button();
+            btnSomar = new Button();
+            btnIgual = new Button();
+            btn0 = new Button();
+            btnLimpar = new Button();
+            panCalculo = new Panel();
+            txtCalculo = new TextBox();
+            txtNumero = new TextBox();
+            tableLayoutPanel1.SuspendLayout();
+            panCalculo.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            tableLayoutPanel1.ColumnCount = 4;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.Controls.Add(btn7, 0, 0);
+            tableLayoutPanel1.Controls.Add(btn8, 1, 0);
+            tableLayoutPanel1.Controls.Add(btn9, 2, 0);
+            tableLayoutPanel1.Controls.Add(btnDividir, 3, 0);
+            tableLayoutPanel1.Controls.Add(btn4, 0, 1);
+            tableLayoutPanel1.Controls.Add(btn5, 1, 1);
+            tableLayoutPanel1.Controls.Add(btn6, 2, 1);
+            tableLayoutPanel1.Controls.Add(btnMultiplicar, 3, 1);
+            tableLayoutPanel1.Controls.Add(btn1, 0, 2);
+            tableLayoutPanel1.Controls.Add(btn3, 2, 2);
+            tableLayoutPanel1.Controls.Add(btn2, 1, 2);
+            tableLayoutPanel1.Controls.Add(btnSubtrair, 3, 2);
+            tableLayoutPanel1.Controls.Add(btnSomar, 3, 3);
+            tableLayoutPanel1.Controls.Add(btnIgual, 2, 3);
+            tableLayoutPanel1.Controls.Add(btn0, 1, 3);
+            tableLayoutPanel1.Controls.Add(btnLimpar, 0, 3);
+            tableLayoutPanel1.Location = new Point(12, 167);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 4;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.Size = new Size(427, 377);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // btn7
+            // 
+            btn7.BackColor = Color.FromArgb(96, 96, 96);
+            btn7.Dock = DockStyle.Fill;
+            btn7.FlatAppearance.BorderSize = 0;
+            btn7.FlatStyle = FlatStyle.Flat;
+            btn7.Font = new Font("Verdana", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            btn7.ForeColor = Color.White;
+            btn7.Location = new Point(3, 3);
+            btn7.Name = "btn7";
+            btn7.Size = new Size(100, 88);
+            btn7.TabIndex = 0;
+            btn7.Text = "7";
+            btn7.UseVisualStyleBackColor = false;
+            // 
+            // btn8
+            // 
+            btn8.BackColor = Color.FromArgb(96, 96, 96);
+            btn8.Dock = DockStyle.Fill;
+            btn8.FlatAppearance.BorderSize = 0;
+            btn8.FlatStyle = FlatStyle.Flat;
+            btn8.Font = new Font("Verdana", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            btn8.ForeColor = Color.White;
+            btn8.Location = new Point(109, 3);
+            btn8.Name = "btn8";
+            btn8.Size = new Size(100, 88);
+            btn8.TabIndex = 1;
+            btn8.Text = "8";
+            btn8.UseVisualStyleBackColor = false;
+            // 
+            // btn9
+            // 
+            btn9.BackColor = Color.FromArgb(96, 96, 96);
+            btn9.Dock = DockStyle.Fill;
+            btn9.FlatAppearance.BorderSize = 0;
+            btn9.FlatStyle = FlatStyle.Flat;
+            btn9.Font = new Font("Verdana", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            btn9.ForeColor = Color.White;
+            btn9.Location = new Point(215, 3);
+            btn9.Name = "btn9";
+            btn9.Size = new Size(100, 88);
+            btn9.TabIndex = 2;
+            btn9.Text = "9";
+            btn9.UseVisualStyleBackColor = false;
+            // 
+            // btnDividir
+            // 
+            btnDividir.BackColor = Color.FromArgb(255, 158, 7);
+            btnDividir.Dock = DockStyle.Fill;
+            btnDividir.FlatAppearance.BorderColor = Color.FromArgb(255, 158, 7);
+            btnDividir.FlatAppearance.BorderSize = 0;
+            btnDividir.FlatStyle = FlatStyle.Flat;
+            btnDividir.Font = new Font("Verdana", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            btnDividir.ForeColor = Color.White;
+            btnDividir.Location = new Point(321, 3);
+            btnDividir.Name = "btnDividir";
+            btnDividir.Size = new Size(103, 88);
+            btnDividir.TabIndex = 3;
+            btnDividir.Text = "/";
+            btnDividir.UseVisualStyleBackColor = false;
+            // 
+            // btn4
+            // 
+            btn4.BackColor = Color.FromArgb(96, 96, 96);
+            btn4.Dock = DockStyle.Fill;
+            btn4.FlatAppearance.BorderSize = 0;
+            btn4.FlatStyle = FlatStyle.Flat;
+            btn4.Font = new Font("Verdana", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            btn4.ForeColor = Color.White;
+            btn4.Location = new Point(3, 97);
+            btn4.Name = "btn4";
+            btn4.Size = new Size(100, 88);
+            btn4.TabIndex = 4;
+            btn4.Text = "4";
+            btn4.UseVisualStyleBackColor = false;
+            // 
+            // btn5
+            // 
+            btn5.BackColor = Color.FromArgb(96, 96, 96);
+            btn5.Dock = DockStyle.Fill;
+            btn5.FlatAppearance.BorderSize = 0;
+            btn5.FlatStyle = FlatStyle.Flat;
+            btn5.Font = new Font("Verdana", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            btn5.ForeColor = Color.White;
+            btn5.Location = new Point(109, 97);
+            btn5.Name = "btn5";
+            btn5.Size = new Size(100, 88);
+            btn5.TabIndex = 5;
+            btn5.Text = "5";
+            btn5.UseVisualStyleBackColor = false;
+            // 
+            // btn6
+            // 
+            btn6.BackColor = Color.FromArgb(96, 96, 96);
+            btn6.Dock = DockStyle.Fill;
+            btn6.FlatAppearance.BorderSize = 0;
+            btn6.FlatStyle = FlatStyle.Flat;
+            btn6.Font = new Font("Verdana", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            btn6.ForeColor = Color.White;
+            btn6.Location = new Point(215, 97);
+            btn6.Name = "btn6";
+            btn6.Size = new Size(100, 88);
+            btn6.TabIndex = 6;
+            btn6.Text = "6";
+            btn6.UseVisualStyleBackColor = false;
+            // 
+            // btnMultiplicar
+            // 
+            btnMultiplicar.BackColor = Color.FromArgb(255, 158, 7);
+            btnMultiplicar.Dock = DockStyle.Fill;
+            btnMultiplicar.FlatAppearance.BorderColor = Color.FromArgb(255, 158, 7);
+            btnMultiplicar.FlatAppearance.BorderSize = 0;
+            btnMultiplicar.FlatStyle = FlatStyle.Flat;
+            btnMultiplicar.Font = new Font("Verdana", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            btnMultiplicar.ForeColor = Color.White;
+            btnMultiplicar.Location = new Point(321, 97);
+            btnMultiplicar.Name = "btnMultiplicar";
+            btnMultiplicar.Size = new Size(103, 88);
+            btnMultiplicar.TabIndex = 7;
+            btnMultiplicar.Text = "*";
+            btnMultiplicar.UseVisualStyleBackColor = false;
+            // 
+            // btn1
+            // 
+            btn1.BackColor = Color.FromArgb(96, 96, 96);
+            btn1.Dock = DockStyle.Fill;
+            btn1.FlatAppearance.BorderSize = 0;
+            btn1.FlatStyle = FlatStyle.Flat;
+            btn1.Font = new Font("Verdana", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            btn1.ForeColor = Color.White;
+            btn1.Location = new Point(3, 191);
+            btn1.Name = "btn1";
+            btn1.Size = new Size(100, 88);
+            btn1.TabIndex = 8;
+            btn1.Text = "1";
+            btn1.UseVisualStyleBackColor = false;
+            // 
+            // btn3
+            // 
+            btn3.BackColor = Color.FromArgb(96, 96, 96);
+            btn3.Dock = DockStyle.Fill;
+            btn3.FlatAppearance.BorderSize = 0;
+            btn3.FlatStyle = FlatStyle.Flat;
+            btn3.Font = new Font("Verdana", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            btn3.ForeColor = Color.White;
+            btn3.Location = new Point(215, 191);
+            btn3.Name = "btn3";
+            btn3.Size = new Size(100, 88);
+            btn3.TabIndex = 9;
+            btn3.Text = "3";
+            btn3.UseVisualStyleBackColor = false;
+            // 
+            // btn2
+            // 
+            btn2.BackColor = Color.FromArgb(96, 96, 96);
+            btn2.Dock = DockStyle.Fill;
+            btn2.FlatAppearance.BorderSize = 0;
+            btn2.FlatStyle = FlatStyle.Flat;
+            btn2.Font = new Font("Verdana", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            btn2.ForeColor = Color.White;
+            btn2.Location = new Point(109, 191);
+            btn2.Name = "btn2";
+            btn2.Size = new Size(100, 88);
+            btn2.TabIndex = 10;
+            btn2.Text = "2";
+            btn2.UseVisualStyleBackColor = false;
+            // 
+            // btnSubtrair
+            // 
+            btnSubtrair.BackColor = Color.FromArgb(255, 158, 7);
+            btnSubtrair.Dock = DockStyle.Fill;
+            btnSubtrair.FlatAppearance.BorderColor = Color.FromArgb(255, 158, 7);
+            btnSubtrair.FlatAppearance.BorderSize = 0;
+            btnSubtrair.FlatStyle = FlatStyle.Flat;
+            btnSubtrair.Font = new Font("Verdana", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            btnSubtrair.ForeColor = Color.White;
+            btnSubtrair.Location = new Point(321, 191);
+            btnSubtrair.Name = "btnSubtrair";
+            btnSubtrair.Size = new Size(103, 88);
+            btnSubtrair.TabIndex = 11;
+            btnSubtrair.Text = "-";
+            btnSubtrair.UseVisualStyleBackColor = false;
+            // 
+            // btnSomar
+            // 
+            btnSomar.BackColor = Color.FromArgb(255, 158, 7);
+            btnSomar.Dock = DockStyle.Fill;
+            btnSomar.FlatAppearance.BorderColor = Color.FromArgb(255, 158, 7);
+            btnSomar.FlatAppearance.BorderSize = 0;
+            btnSomar.FlatStyle = FlatStyle.Flat;
+            btnSomar.Font = new Font("Verdana", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            btnSomar.ForeColor = Color.White;
+            btnSomar.Location = new Point(321, 285);
+            btnSomar.Name = "btnSomar";
+            btnSomar.Size = new Size(103, 89);
+            btnSomar.TabIndex = 12;
+            btnSomar.Text = "+";
+            btnSomar.UseVisualStyleBackColor = false;
+            // 
+            // btnIgual
+            // 
+            btnIgual.BackColor = Color.FromArgb(65, 65, 65);
+            btnIgual.Dock = DockStyle.Fill;
+            btnIgual.FlatAppearance.BorderSize = 0;
+            btnIgual.FlatStyle = FlatStyle.Flat;
+            btnIgual.Font = new Font("Verdana", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            btnIgual.ForeColor = Color.White;
+            btnIgual.Location = new Point(215, 285);
+            btnIgual.Name = "btnIgual";
+            btnIgual.Size = new Size(100, 89);
+            btnIgual.TabIndex = 13;
+            btnIgual.Text = "=";
+            btnIgual.UseVisualStyleBackColor = false;
+            // 
+            // btn0
+            // 
+            btn0.BackColor = Color.FromArgb(65, 65, 65);
+            btn0.Dock = DockStyle.Fill;
+            btn0.FlatAppearance.BorderSize = 0;
+            btn0.FlatStyle = FlatStyle.Flat;
+            btn0.Font = new Font("Verdana", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            btn0.ForeColor = Color.White;
+            btn0.Location = new Point(109, 285);
+            btn0.Name = "btn0";
+            btn0.Size = new Size(100, 89);
+            btn0.TabIndex = 14;
+            btn0.Text = "0";
+            btn0.UseVisualStyleBackColor = false;
+            // 
+            // btnLimpar
+            // 
+            btnLimpar.BackColor = Color.FromArgb(65, 65, 65);
+            btnLimpar.Dock = DockStyle.Fill;
+            btnLimpar.FlatAppearance.BorderSize = 0;
+            btnLimpar.FlatStyle = FlatStyle.Flat;
+            btnLimpar.Font = new Font("Verdana", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            btnLimpar.ForeColor = Color.White;
+            btnLimpar.Location = new Point(3, 285);
+            btnLimpar.Name = "btnLimpar";
+            btnLimpar.Size = new Size(100, 89);
+            btnLimpar.TabIndex = 15;
+            btnLimpar.Text = "CE";
+            btnLimpar.UseVisualStyleBackColor = false;
+            // 
+            // panCalculo
+            // 
+            panCalculo.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            panCalculo.BackColor = Color.FromArgb(69, 69, 69);
+            panCalculo.Controls.Add(txtCalculo);
+            panCalculo.Controls.Add(txtNumero);
+            panCalculo.Location = new Point(13, 7);
+            panCalculo.Name = "panCalculo";
+            panCalculo.Size = new Size(426, 157);
+            panCalculo.TabIndex = 1;
+            // 
+            // txtCalculo
+            // 
+            txtCalculo.Dock = DockStyle.Top;
+            txtCalculo.Font = new Font("Verdana", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            txtCalculo.Location = new Point(0, 0);
+            txtCalculo.Name = "txtCalculo";
+            txtCalculo.Size = new Size(426, 40);
+            txtCalculo.TabIndex = 1;
+            // 
+            // txtNumero
+            // 
+            txtNumero.Dock = DockStyle.Bottom;
+            txtNumero.Font = new Font("Verdana", 48F, FontStyle.Bold, GraphicsUnit.Point);
+            txtNumero.Location = new Point(0, 52);
+            txtNumero.Name = "txtNumero";
+            txtNumero.Size = new Size(426, 105);
+            txtNumero.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(45, 45, 45);
+            ClientSize = new Size(451, 556);
+            Controls.Add(panCalculo);
+            Controls.Add(tableLayoutPanel1);
+            MinimumSize = new Size(469, 506);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Calculadora da Taize";
+            tableLayoutPanel1.ResumeLayout(false);
+            panCalculo.ResumeLayout(false);
+            panCalculo.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private Button btn7;
+        private Button btn8;
+        private Button btn9;
+        private Button btnDividir;
+        private Button btn4;
+        private Button btn5;
+        private Button btn6;
+        private Button btnMultiplicar;
+        private Button btn1;
+        private Button btn3;
+        private Button btn2;
+        private Button btnSubtrair;
+        private Button btnSomar;
+        private Button btnIgual;
+        private Button btn0;
+        private Button btnLimpar;
+        private Panel panCalculo;
+        private TextBox txtCalculo;
+        private TextBox txtNumero;
     }
 }
